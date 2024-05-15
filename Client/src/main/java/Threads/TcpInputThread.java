87710bb6b6897fb7c6f98ea07c7abcff46054086
LJQ <82761283+Seeking-L@users.noreply.github.com>
2024-05-15 09:50:03 +0800

    add server2

---
+++ Client/src/main/java/Threads/TcpInputThread.java
@@ -0,0 +1,75 @@
+package Threads;
+
+import Entity.CommunicationRequest;
+import Entity.TextMessageFromClient;
+import Entity.User;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * 此线程用于：用户登录成功后，与朋友联系上之前。即用来不断扫描用户选择好友的输入
+ */
+public class WaitSelectFriend implements Runnable{
+    private LinkedList<User> communicatingFriend;
+    private Scanner scanner;
+    private ObjectInputStream tcpIn;
+    private ObjectOutputStream tcpOut;
+    public LinkedList<User> friends;//friend list
+
+    public WaitSelectFriend(LinkedList<User> communicatingFriend,Scanner scanner, ObjectInputStream tcpIn, ObjectOutputStream tcpOut, LinkedList<User> friends) {
+        this.communicatingFriend=communicatingFriend;
+        this.scanner = scanner;
+        this.tcpIn = tcpIn;
+        this.tcpOut = tcpOut;
+        this.friends = friends;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.interrupted()){
+            OUT1:
+            try {
+                if(scanner.hasNext()){
+                    if(communicatingFriend.isEmpty()) {//还未联系上好友
+                        if (scanner.hasNextInt()) {
+                            int friendId = scanner.nextInt();
+                            for (User f : friends
+                            ) {
+                                if (f.getUserId() == friendId) {//尝试联络好友
+                                    tcpOut.writeObject(new CommunicationRequest(friendId));
+                                    tcpOut.flush();
+                                    System.out.println("正在等待服务器回信，请稍候~");
+                                    break OUT1;
+                                }
+                            }
+                            System.out.println("无此ID的好友~");
+                        } else {
+                            scanner.nextLine();
+                            System.out.println("若希望联系好友，请输入好友的ID~");
+                        }
+                    }else {//已联系上好友
+                        System.out.println();
+                        String message=scanner.nextLine();
+                        if(message.equals("/bye")){//结束程序
+                            System.exit(0);
+                        }
+                        TextMessageFromClient textMessageFromClient=new TextMessageFromClient(communicatingFriend.get(0).getUserId(),message);
+                        try {
+                            tcpOut.writeObject(textMessageFromClient);
+                            tcpOut.flush();
+                        }catch (IOException e){
+                            System.out.println("--------------------信息发送失败！--------------------");
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
